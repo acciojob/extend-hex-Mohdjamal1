7ad060064bee1394df8263727f89da0631e56baa
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-06-10 12:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let str = '#';
+	if (shortHex.charAt(0) === '#') {
+		for (let i = 1; i < shortHex.length; i++) {
+			str += shortHex.charAt(i) + shortHex.charAt(i);
+		}
+	}
+	else{
+		for (let i = 0; i < shortHex.length; i++) {
+			str += shortHex.charAt(i) + shortHex.charAt(i);
+		}
+	}
+	// 
+	return str;
 };
 
 // Do not change the code below.
